02c2c9086d9dcf6775baedb31781e23ad35a53e1
ggrangga <32285142+ggrangga@users.noreply.github.com>
2018-04-02 13:50:22 +0700

    Update UpdateCostDistGoogleApi.java

--- src/java/com/fz/tms/params/Backgrounder/UpdateCostDistGoogleApi.java
+++ src/java/com/fz/tms/params/Backgrounder/UpdateCostDistGoogleApi.java
@@ -6,6 +6,10 @@
 package com.fz.tms.params.Backgrounder;
 
 import com.fz.generic.Db;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -18,17 +22,20 @@ import java.util.List;
  * @author dwi.oktaviandi
  */
 public class UpdateCostDistGoogleApi {
-    private List<HashMap<String, String>> cust = new ArrayList<HashMap<String, String>>();
+    
     public List<HashMap<String, String>> finalizeCust() throws Exception{
         List<HashMap<String, String>> px = new ArrayList<HashMap<String, String>>();
         HashMap<String, String> py = new HashMap<String, String>();
 
-        
-        cust = getDataCust("");
+        String branch = "D312";
+        List<HashMap<String, String>> cust = new ArrayList<HashMap<String, String>>();
+        cust = getDataCust(branch);
         List<HashMap<String, String>> shipment = new ArrayList<HashMap<String, String>>();
-        shipment = getShipment("");
+        shipment = getShipment(branch);
         List<HashMap<String, String>> costdist = new ArrayList<HashMap<String, String>>();
-        costdist = getCostDist("");
+        costdist = getCostDist(branch);
+        
+        int send = 25;
 
         System.out.println(cust.size() + "getShipment ()" + shipment.size() + "getCostDist ()" + costdist.size());
 
@@ -37,9 +44,8 @@ public class UpdateCostDistGoogleApi {
         while(x < cust.size()){
             int y = 0;
             py = new HashMap<String, String>();
-            py.put("customer_id", cust.get(x).get("customer_id"));
             Boolean find = false;
-            //cek data udah ada di costdict
+            //cek data udah ada di costdist
             while(y < costdist.size()){
                 if(!cust.get(x).get("customer_id").equalsIgnoreCase(costdist.get(y).get("customer_id"))){
                     find = false;
@@ -68,6 +74,7 @@ public class UpdateCostDistGoogleApi {
                     z++;
                 }
                 if(py.get("priority").equalsIgnoreCase("1")){
+                    py.put("customer_id", cust.get(x).get("customer_id"));            
                     py.put("Long", cust.get(x).get("Long"));
                     py.put("Lat", cust.get(x).get("Lat"));
                     System.out.println("()" + py.toString());
@@ -80,13 +87,53 @@ public class UpdateCostDistGoogleApi {
         }
         System.out.println(px.size());
 
-        x = 0;
-        while(x < px.size()){
-            //if(px.get(x).get("priority").equalsIgnoreCase("1")){
-                //System.out.println(px.get(x).toString());
-            //}
+        List<HashMap<String, String>> pz = new ArrayList<HashMap<String, String>>();
+        String urlString = "";/*
+            "https://maps.googleapis.com/maps/api/distancematrix/json"
+                + "?origins=" + origLat + "," + origLon
+                + "&destinations=" + destList
+                + "&departure_time=now"
+                + "&traffic_model=best_guess"
+                ;*/
+        String key = "AIzaSyBOsad8CCGx7acE9H_c-27JVH-qqKzei20";
+        
+        URL url = new URL(urlString + "&key=" + key);
+        String finalURL = url.toString();
+        URL obj = new URL(finalURL);
+        
+        try{
+            HttpURLConnection htCon = (HttpURLConnection) obj.openConnection();
+            htCon.setRequestMethod("GET");
+            htCon.setRequestProperty("User-Agent", "Mozilla/5.0");
+            String resultJson = "";
+            try (BufferedReader in = new BufferedReader(
+                    new InputStreamReader(htCon.getInputStream()))){
+                String inputLine;
+                StringBuffer response = new StringBuffer();
+                while ((inputLine = in.readLine()) != null) {
+                        response.append(inputLine);
+                }
+                in.close();
+                resultJson = response.toString();
+                System.out.println("resultJson : " + resultJson);
+            }
+        }catch(Exception e){
             
-            x++;
+        }
+        
+        while(pz.size() < send){
+            x = 0;
+            while(x < px.size()){
+                int y = 0;
+                while(y < px.size()){
+                    py = new HashMap<String, String>();
+                    if(!px.get(x).get("customer_id").equalsIgnoreCase(px.get(y).get("customer_id"))){
+                        
+                    }
+                    y++;
+                }
+                x++;
+            }            
         }
 
         return px;
@@ -95,8 +142,6 @@ public class UpdateCostDistGoogleApi {
     public List<HashMap<String, String>> getDataCust(String branch) throws Exception{
         List<HashMap<String, String>> px = new ArrayList<HashMap<String, String>>();
         HashMap<String, String> py = new HashMap<String, String>();
-
-        branch = "D312";
 
         String sql = "SELECT\n" +
             "	DISTINCT aq.customer_id,\n" +
@@ -138,8 +183,6 @@ public class UpdateCostDistGoogleApi {
         List<HashMap<String, String>> px = new ArrayList<HashMap<String, String>>();
         HashMap<String, String> py = new HashMap<String, String>();
 
-        branch = "D312";
-
         String sql = "select distinct Customer_ID from BOSNET1.dbo.TMS_ShipmentPlan where Plant = '"+branch+"'";
 
         try (Connection con = (new Db()).getConnection("jdbc/fztms");
@@ -158,8 +201,6 @@ public class UpdateCostDistGoogleApi {
     public List<HashMap<String, String>> getCostDist(String branch) throws Exception{
         List<HashMap<String, String>> px = new ArrayList<HashMap<String, String>>();
         HashMap<String, String> py = new HashMap<String, String>();
-
-        branch = "D312";
 
         String sql = "select distinct SUBSTRING(from1, 1, 10) as from1 from BOSNET1.dbo.TMS_CostDist where branch = '"+branch+"'";
 
